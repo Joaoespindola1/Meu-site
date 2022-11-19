d684e68c4672371af49698ee09745e028ae2876e
João Pedro Espindola <102266728+Joaoespindola1@users.noreply.github.com>
2022-11-19 00:32:48 -0300

    Adicionando arquivos

---
+++ js/scrollReveal.js
@@ -0,0 +1,45 @@
+ScrollReveal({reset: true})
+
+ScrollReveal().reveal( '.text', {
+    delay:300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+    }
+});
+ScrollReveal().reveal( '.text2', {
+    delay:300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+    }
+});
+ScrollReveal().reveal( '.text3', {
+    delay:400,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+    }
+});
+ScrollReveal().reveal( '.contato', {
+    delay:300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+    }
+});
+ScrollReveal().reveal( '.inicio', {
+    delay:300,
+    rotate:{
+        x:100,
+        y:0,
+        z:0,
+    }
+});
+
+
+
